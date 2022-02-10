4790da3db3da22faf99551018db84c4e8cdd2707
dmitrykachala <kosas@mail.ru>
2022-02-10 16:38:39 +0300

    0. ToOne [#6873]

--- src/main/java/ru/job4j/todo/store/TaskStore.java
+++ src/main/java/ru/job4j/todo/store/TaskStore.java
@@ -1,6 +1,7 @@
 package ru.job4j.todo.store;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
@@ -8,16 +9,10 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.todo.model.Task;
 import ru.job4j.todo.model.User;
-
-import javax.persistence.Query;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TaskStore implements AutoCloseable {
@@ -152,11 +147,10 @@ public class TaskStore implements AutoCloseable {
             Query query = session
                     .createQuery("from ru.job4j.todo.model.User where email = :email");
             query.setParameter("email", email);
-            Optional res = query.getResultList().stream().findFirst();
-            User result = res.isPresent() ? (User) res.get() : null;
+
             session.getTransaction().commit();
             session.close();
-            return result;
+            return (User) query.uniqueResult();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -181,12 +175,6 @@ public class TaskStore implements AutoCloseable {
         } finally {
             session.close();
         }
-    }
-
-    public static void main(String[] args) {
-        TaskStore store = TaskStore.getInstance();
-        System.out.println(store.findUserByEmail("11"));
-        System.out.println(store.findAll());
     }
 
 }
